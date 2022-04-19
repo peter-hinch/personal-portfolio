d43d114e1df906b91cac108a040d2f6f4f834759
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-04-20 09:29:05 +1000

    Create hard-coded array of portfolio items.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import PortfolioSection from './components/PortfolioSection';
 import PortfolioItem from './components/PortfolioItem';
 
+const portfolioItems = [
+  {
+    heading: 'RemoteSpot'
+  },
+  {
+    heading: 'React Todo List'
+  },
+  {
+    heading: 'Web Audio API Piano'
+  },
+  {
+    heading: 'SD Perfumery'
+  }
+];
+
 function App() {
-  return (
-    <PortfolioSection>
-      <PortfolioItem />
-    </PortfolioSection>
-  );
+  const renderPortfolioItems = portfolioItems.map((item) => (
+    <PortfolioItem heading={item.heading} />
+  ));
+
+  return <PortfolioSection>{renderPortfolioItems}</PortfolioSection>;
 }
 
 export default App;
