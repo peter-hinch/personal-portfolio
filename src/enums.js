3a2fade60a37d14e54e98d067d34c609db0420dc
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-04-20 11:12:08 +1000

    Convert enums to objects, add suffix for correct Devicon image.

--- src/enums.js
+++ src/enums.js
@@ -1,20 +1,32 @@
+// Technology icons are from Devicon
+// Reference: https://devicon.dev/
+
+// Some icons are more readily identifiable with a wordmark, the 'iconSuffix'
+// key for each enum indicates which icon to use for each technology.
+
 export const Technologies = {
-  AMAZONWEBSERVICES: 'amazonwebservices',
-  ANDROID: 'android',
-  AZURE: 'azure',
-  BOOTSTRAP: 'bootstrap',
-  CSS3: 'css3',
-  CSHARP: 'csharp',
-  DOT_NET: 'dot-net',
-  HTML5: 'html5',
-  FIGMA: 'figma',
-  JAVA: 'java',
-  JAVASCRIPT: 'javascript',
-  MICROSOFTSQLSERVER: 'microsoftsqlserver',
-  NODEJS: 'nodejs',
-  REACT: 'react',
-  SASS: 'sass',
-  MYSQL: 'mysql',
-  TYPESCRIPT: 'typescript',
-  WEBPACK: 'webpack'
+  AMAZONWEBSERVICES: {
+    simplename: 'amazonwebservices',
+    iconSuffix: '-original-wordmark'
+  },
+  ANDROID: { simplename: 'android', iconSuffix: '-original' },
+  AZURE: { simplename: 'azure', iconSuffix: '-original-wordmark' },
+  BOOTSTRAP: { simplename: 'bootstrap', iconSuffix: '-original' },
+  CSHARP: { simplename: 'csharp', iconSuffix: '-original' },
+  CSS3: { simplename: 'css3', iconSuffix: '-original-wordmark' },
+  DOT_NET: { simplename: 'dot-net', iconSuffix: '-original-wordmark' },
+  FIGMA: { simplename: 'figma', iconSuffix: '-original' },
+  HTML5: { simplename: 'html5', iconSuffix: '-original-wordmark' },
+  JAVA: { simplename: 'java', iconSuffix: '-original-wordmark' },
+  JAVASCRIPT: { simplename: 'javascript', iconSuffix: '-original' },
+  MICROSOFTSQLSERVER: {
+    simplename: 'microsoftsqlserver',
+    iconSuffix: '-plain-wordmark'
+  },
+  MYSQL: { simplename: 'mysql', iconSuffix: '-original-wordmark' },
+  NODEJS: { simplename: 'nodejs', iconSuffix: '-original-wordmark' },
+  REACT: { simplename: 'react', iconSuffix: '-original' },
+  SASS: { simplename: 'sass', iconSuffix: '-original' },
+  TYPESCRIPT: { simplename: 'typescript', iconSuffix: '-original' },
+  WEBPACK: { simplename: 'webpack', iconSuffix: '-original' }
 };
