46eb395ca97407c53d2b3c6a2a2295b323ad4d1e
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-04-21 00:56:47 +1000

    Add component AboutMe.

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,21 @@
+const AboutMe = () => {
+  return (
+    <section className="about-me">
+      <h2>AboutMe</h2>
+      <p>
+        I am a Frontend Developer with professional experience in the
+        construction and manufacturing industries, with a great capability in
+        the JavaScript scripting language, HTML and CSS.
+      </p>
+
+      <p>
+        Currently studying React Development at General Assembly Melbourne.
+        Passionate about designing user-friendly content and with a knack for
+        taking a lateral approach - blending technical and creative aspects and
+        proven ability to produce engaging user-facing experiences.
+      </p>
+    </section>
+  );
+};
+
+export default AboutMe;
