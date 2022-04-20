ebaa7524ef51a46764538fe76d063ccf86699e32
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-04-21 01:40:16 +1000

    Add placeholders for links.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,19 @@
 import Container from './layout/Container';
 
+const currentYear = new Date().getFullYear();
+
 const Footer = () => {
-  return <Container>Footer</Container>;
+  return (
+    <Container>
+      <nav>
+        <ul>
+          <li>GitHub</li>
+          <li>LinkedIn</li>
+        </ul>
+        <small>&copy;{currentYear} Peter Hinch, All rights reserved.</small>
+      </nav>
+    </Container>
+  );
 };
 
 export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,18 @@
 import Container from './layout/Container';
 
 const Navbar = () => {
-  return <Container>Navbar</Container>;
+  return (
+    <Container>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About Me</li>
+          <li>Technologies</li>
+          <li>Portfolio</li>
+        </ul>
+      </nav>
+    </Container>
+  );
 };
 
 export default Navbar;
