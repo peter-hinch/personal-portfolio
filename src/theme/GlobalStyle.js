b148cecca9b6213b4318354734bef02b3376b415
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-04-21 01:35:52 +1000

    Add media queries for responsive container widths.

--- src/theme/GlobalStyle.js
+++ src/theme/GlobalStyle.js
@@ -1,5 +1,28 @@
 import { createGlobalStyle } from 'styled-components';
 
-const GlobalStyle = createGlobalStyle``;
+const GlobalStyle = createGlobalStyle`
+  @media only screen and (max-width: 727px) {
+  }
+
+  @media only screen and (min-width: 728px) {
+    .container {
+      width: 42.5rem;
+    }
+  }
+
+  @media only screen and (min-width: 952px) {
+    .container {
+      width: 56.5rem;
+    }
+  }
+  
+  @media only screen and (min-width: 1201px) {
+    .container {
+      width: 72rem;
+    }
+  }
+
+
+`;
 
 export default GlobalStyle;
