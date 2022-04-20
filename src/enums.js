1c1e736114cc50d873d689cd54ff794a990dc1e8
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-04-20 10:33:50 +1000

    Create file for enum definitions,  enums.js .

---
+++ src/enums.js
@@ -0,0 +1,16 @@
+export const Technologies = {
+  BOOTSTRAP: 'bootstrap',
+  CSS3: 'css3',
+  CSHARP: 'csharp',
+  HTML5: 'html5',
+  FIGMA: 'figma',
+  JAVA: 'java',
+  JAVASCRIPT: 'javascript',
+  MICROSOFTSQLSERVER: 'microsoftsqlserver',
+  NODEJS: 'nodejs',
+  REACT: 'react',
+  SASS: 'sass',
+  MYSQL: 'mysql',
+  TYPESCRIPT: 'typescript',
+  WEBPACK: 'webpack'
+};
