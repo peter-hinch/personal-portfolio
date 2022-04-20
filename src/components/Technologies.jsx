6fc8f013d77e5214f299247fe6536ba65a7b3340
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-04-21 02:25:04 +1000

    Render icons for preferred technologies.

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,7 +1,24 @@
-const Technologies = () => {
+const iconCdnUrl = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/';
+
+const Technologies = ({ preferredTechnologies }) => {
+  const renderPreferredTechnologies = preferredTechnologies.map(
+    (technology) => (
+      <span>
+        <img
+          src={`${iconCdnUrl}${technology.simplename}/${technology.simplename}${technology.iconSuffix}.svg`}
+          alt={technology.simplename}
+          height={64}
+        />
+      </span>
+    )
+  );
+
   return (
     <section id="technologies">
       <h2>Technologies</h2>
+      <h3>Preferred Technologies</h3>
+      {renderPreferredTechnologies}
+      <h3>Other Experience</h3>
     </section>
   );
 };
