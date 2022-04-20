8f21deb2f47665b548a57559516456208d423f58
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-04-21 02:52:42 +1000

    Add component Contact.

---
+++ src/components/Contact.jsx
@@ -0,0 +1,10 @@
+const Contact = () => {
+  return (
+    <section id="contact">
+      <h2>Contact</h2>
+      <p>I'd love to hear from you. Please get in touch!</p>
+    </section>
+  );
+};
+
+export default Contact;
