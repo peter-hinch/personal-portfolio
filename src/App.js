4b6c4587b8325be01bcd58501a2162c425d7dbfe
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-04-20 10:51:55 +1000

    Move portfolioItems to new file portfolioData.js .

--- src/App.js
+++ src/App.js
@@ -1,50 +1,6 @@
 import PortfolioSection from './components/PortfolioSection';
 import PortfolioItem from './components/PortfolioItem';
-import { Technologies } from './enums';
-
-const portfolioItems = [
-  {
-    title: 'RemoteSpot',
-    subtitle: 'Ratings application in React',
-    technologies: [
-      Technologies.JAVASCRIPT,
-      Technologies.REACT,
-      Technologies.HTML5,
-      Technologies.CSS3
-    ]
-  },
-  {
-    title: 'Todo List',
-    subtitle: 'Notetaking application in React',
-    technologies: [
-      Technologies.JAVASCRIPT,
-      Technologies.REACT,
-      Technologies.HTML5,
-      Technologies.CSS3
-    ]
-  },
-  {
-    title: 'Piano',
-    subtitle: 'An exercise in using the Web Audio API',
-    technologies: [
-      Technologies.JAVASCRIPT,
-      Technologies.HTML5,
-      Technologies.CSS3
-    ]
-  },
-  {
-    title: 'SD Perfumery',
-    subtitle: 'Storefront developed using .NET MVC',
-    technologies: [
-      Technologies.CSHARP,
-      Technologies.DOT_NET,
-      Technologies.MICROSOFTSQLSERVER,
-      Technologies.BOOTSTRAP,
-      Technologies.HTML5,
-      Technologies.CSS3
-    ]
-  }
-];
+import { portfolioItems } from './portfolioData';
 
 function App() {
   const renderPortfolioItems = portfolioItems.map((item) => (
---
+++ src/portfolioData.js
@@ -0,0 +1,46 @@
+import { Technologies } from './enums';
+
+export const portfolioItems = [
+  {
+    title: 'RemoteSpot',
+    subtitle: 'Ratings application in React',
+    technologies: [
+      Technologies.JAVASCRIPT,
+      Technologies.REACT,
+      Technologies.HTML5,
+      Technologies.CSS3
+    ]
+  },
+  {
+    title: 'Todo List',
+    subtitle: 'Notetaking application in React',
+    technologies: [
+      Technologies.JAVASCRIPT,
+      Technologies.REACT,
+      Technologies.HTML5,
+      Technologies.CSS3
+    ]
+  },
+  {
+    title: 'Piano',
+    subtitle: 'An exercise in using the Web Audio API',
+    technologies: [
+      Technologies.JAVASCRIPT,
+      Technologies.HTML5,
+      Technologies.CSS3
+    ]
+  },
+  {
+    title: 'SD Perfumery',
+    subtitle: 'Storefront developed using .NET MVC',
+    technologies: [
+      Technologies.CSHARP,
+      Technologies.DOT_NET,
+      Technologies.MICROSOFTSQLSERVER,
+      Technologies.AZURE,
+      Technologies.BOOTSTRAP,
+      Technologies.HTML5,
+      Technologies.CSS3
+    ]
+  }
+];
