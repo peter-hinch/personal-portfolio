f2eac87598994f7e44aff1e6ef986a21d5f94dc8
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-04-21 01:11:49 +1000

    Wrap App component with styled components GlobalStyle wrapper.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './normalize.css';
 import App from './App';
+import './normalize.css';
+import GlobalStyle from './theme/GlobalStyle';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle>
+      <App />
+    </GlobalStyle>
   </React.StrictMode>
 );
 
