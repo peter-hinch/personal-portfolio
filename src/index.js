1fe27b591707d672f7d2e3e3d6a51fe004dba7c1
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-04-21 01:15:39 +1000

    Remove children from GlobalStyle (is not a wrapper).

--- src/index.js
+++ src/index.js
@@ -8,9 +8,8 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GlobalStyle>
-      <App />
-    </GlobalStyle>
+    <GlobalStyle />
+    <App />
   </React.StrictMode>
 );
 
