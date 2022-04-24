99f682e48a605e8b663511e51a025b1cd50ba481
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-04-24 16:52:23 +1000

    Wrap in a <header> element.

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,27 +2,29 @@ import Container from './Container';
 
 const Header = () => {
   return (
-    <Container>
-      <nav>
-        <ul>
-          <li>
-            <a href="#home">Home</a>
-          </li>
-          <li>
-            <a href="#about-me">About Me</a>
-          </li>
-          <li>
-            <a href="#technologies">Technologies</a>
-          </li>
-          <li>
-            <a href="#portfolio">Portfolio</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
-    </Container>
+    <header>
+      <Container>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#about-me">About Me</a>
+            </li>
+            <li>
+              <a href="#technologies">Technologies</a>
+            </li>
+            <li>
+              <a href="#portfolio">Portfolio</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </Container>
+    </header>
   );
 };
 
