e359e08ad4f3915aae47d5566ddc334ce3b912cf
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-04-24 16:52:31 +1000

    Wrap in a <footer> element.

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -4,15 +4,17 @@ const currentYear = new Date().getFullYear();
 
 const Footer = () => {
   return (
-    <Container>
-      <nav>
-        <ul>
-          <li>GitHub</li>
-          <li>LinkedIn</li>
-        </ul>
-        <small>&copy;{currentYear} Peter Hinch, All rights reserved.</small>
-      </nav>
-    </Container>
+    <footer>
+      <Container>
+        <nav>
+          <ul>
+            <li>GitHub</li>
+            <li>LinkedIn</li>
+          </ul>
+          <small>&copy;{currentYear} Peter Hinch, All rights reserved.</small>
+        </nav>
+      </Container>
+    </footer>
   );
 };
 
