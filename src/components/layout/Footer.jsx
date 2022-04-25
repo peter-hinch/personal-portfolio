b08e1e50db6a6808aa348d5b3912260cdbb15e6d
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-04-25 10:31:17 +1000

    Render socialLinks from props.

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,15 +2,18 @@ import Container from './Container';
 
 const currentYear = new Date().getFullYear();
 
-const Footer = () => {
+const Footer = ({ socialLinks }) => {
+  const renderSocialLinks = socialLinks.map((link) => (
+    <li>
+      <a href={link.url}>{link.platform.fullname}</a>
+    </li>
+  ));
+
   return (
     <footer>
       <Container>
         <nav>
-          <ul>
-            <li>GitHub</li>
-            <li>LinkedIn</li>
-          </ul>
+          <ul>{renderSocialLinks}</ul>
           <small>&copy;{currentYear} Peter Hinch, All rights reserved.</small>
         </nav>
       </Container>
