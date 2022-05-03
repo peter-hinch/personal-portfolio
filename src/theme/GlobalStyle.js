3ccc8322d51cf5f99c9fef020c23adb4b8643937
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-05-03 11:13:48 +1000

    Move color variables from id 'root' to document 'root'.

--- src/theme/GlobalStyle.js
+++ src/theme/GlobalStyle.js
@@ -1,6 +1,77 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  :root {
+    --color-white: hsl(0, 0%, 100%);
+    --color-black: hsl(0, 0%, 0%);
+    --color-primary-10: hsl(150, 60%, 8.75%);
+    --color-primary-20: hsl(150, 60%, 17.5%);
+    --color-primary-30: hsl(150, 60%, 26.25%);
+    --color-primary-40: hsl(150, 60%, 35%);
+    --color-primary-50: hsl(150, 60%, 44%);
+    --color-primary-60: hsl(150, 60%, 55%);
+    --color-primary-70: hsl(150, 60%, 66%);
+    --color-primary-80: hsl(150, 60%, 77%);
+    --color-primary-90: hsl(150, 60%, 88%);
+    --color-primary-95: hsl(150, 60%, 94%);
+    --color-primary-99: hsl(150, 60%, 98%);
+    --color-secondary-10: hsl(180, 50%, 8.75%);
+    --color-secondary-20: hsl(180, 50%, 17.5%);
+    --color-secondary-30: hsl(180, 50%, 26.25%);
+    --color-secondary-40: hsl(180, 50%, 35%);
+    --color-secondary-50: hsl(180, 50%, 44%);
+    --color-secondary-60: hsl(180, 50%, 55%);
+    --color-secondary-70: hsl(180, 50%, 66%);
+    --color-secondary-80: hsl(180, 50%, 77%);
+    --color-secondary-90: hsl(180, 50%, 88%);
+    --color-secondary-95: hsl(180, 50%, 94%);
+    --color-secondary-99: hsl(180, 50%, 98%);
+    --color-tertiary-10: hsl(210, 40%, 8.75%);
+    --color-tertiary-20: hsl(210, 40%, 17.5%);
+    --color-tertiary-30: hsl(210, 40%, 26.25%);
+    --color-tertiary-40: hsl(210, 40%, 35%);
+    --color-tertiary-50: hsl(210, 40%, 44%);
+    --color-tertiary-60: hsl(210, 40%, 55%);
+    --color-tertiary-70: hsl(210, 40%, 66%);
+    --color-tertiary-80: hsl(210, 40%, 77%);
+    --color-tertiary-90: hsl(210, 40%, 88%);
+    --color-tertiary-95: hsl(210, 40%, 94%);
+    --color-tertiary-99: hsl(210, 40%, 98%);
+    --color-neutral-10: hsl(165, 4%, 8.75%);
+    --color-neutral-20: hsl(165, 4%, 17.5%);
+    --color-neutral-30: hsl(165, 4%, 26.25%);
+    --color-neutral-40: hsl(165, 4%, 35%);
+    --color-neutral-50: hsl(165, 4%, 44%);
+    --color-neutral-60: hsl(165, 4%, 55%);
+    --color-neutral-70: hsl(165, 4%, 66%);
+    --color-neutral-80: hsl(165, 4%, 77%);
+    --color-neutral-90: hsl(165, 4%, 88%);
+    --color-neutral-95: hsl(165, 4%, 94%);
+    --color-neutral-99: hsl(165, 4%, 98%);
+    --color-neutral-variant-10: hsl(195, 4%, 8.75%);
+    --color-neutral-variant-20: hsl(195, 4%, 17.5%);
+    --color-neutral-variant-30: hsl(195, 4%, 26.25%);
+    --color-neutral-variant-40: hsl(195, 4%, 44%);
+    --color-neutral-variant-50: hsl(188, 2%, 49%);
+    --color-neutral-variant-60: hsl(195, 4%, 55%);
+    --color-neutral-variant-70: hsl(195, 4%, 66%);
+    --color-neutral-variant-80: hsl(195, 4%, 77%);
+    --color-neutral-variant-90: hsl(195, 4%, 88%);
+    --color-neutral-variant-95: hsl(195, 4%, 94%);
+    --color-neutral-variant-99: hsl(195, 4%, 98%);
+    --color-error-10: hsl(0, 40%, 8.75%);
+    --color-error-20: hsl(0, 40%, 17.5%);
+    --color-error-30: hsl(0, 40%, 26.25%);
+    --color-error-40: hsl(0, 40%, 35%);
+    --color-error-50: hsl(0, 40%, 44%);
+    --color-error-60: hsl(0, 40%, 55%);
+    --color-error-70: hsl(0, 40%, 66%);
+    --color-error-80: hsl(0, 40%, 77%);
+    --color-error-90: hsl(0, 40%, 88%);
+    --color-error-95: hsl(0, 40%, 94%);
+    --color-error-99: hsl(0, 40%, 98%);
+  }
+  
   @media only screen and (max-width: 727px) {
   }
 
@@ -26,74 +97,6 @@ const GlobalStyle = createGlobalStyle`
     display: flex;
     flex-direction: column;
     align-items: center;
-    --color-white: hsl(0, 0%, 100%);
-    --color-black: hsl(0, 0%, 0%);
-    --color-primary-10: hsl(240, 40%, 8.75%);
-    --color-primary-20: hsl(240, 40%, 17.5%);
-    --color-primary-30: hsl(240, 40%, 26.25%);
-    --color-primary-40: hsl(240, 40%, 35%);
-    --color-primary-50: hsl(240, 40%, 44%);
-    --color-primary-60: hsl(240, 40%, 55%);
-    --color-primary-70: hsl(240, 40%, 66%);
-    --color-primary-80: hsl(240, 40%, 77%);
-    --color-primary-90: hsl(240, 40%, 88%);
-    --color-primary-95: hsl(240, 40%, 94%);
-    --color-primary-99: hsl(240, 40%, 98%);
-    --color-secondary-10: hsl(220, 40%, 8.75%);
-    --color-secondary-20: hsl(220, 40%, 17.5%);
-    --color-secondary-30: hsl(220, 40%, 26.25%);
-    --color-secondary-40: hsl(220, 40%, 35%);
-    --color-secondary-50: hsl(220, 40%, 44%);
-    --color-secondary-60: hsl(220, 40%, 55%);
-    --color-secondary-70: hsl(220, 40%, 66%);
-    --color-secondary-80: hsl(220, 40%, 77%);
-    --color-secondary-90: hsl(220, 40%, 88%);
-    --color-secondary-95: hsl(220, 40%, 94%);
-    --color-secondary-99: hsl(220, 40%, 98%);
-    --color-tertiary-10: hsl(200, 40%, 8.75%);
-    --color-tertiary-20: hsl(200, 40%, 17.5%);
-    --color-tertiary-30: hsl(200, 40%, 26.25%);
-    --color-tertiary-40: hsl(200, 40%, 35%);
-    --color-tertiary-50: hsl(200, 40%, 44%);
-    --color-tertiary-60: hsl(200, 40%, 55%);
-    --color-tertiary-70: hsl(200, 40%, 66%);
-    --color-tertiary-80: hsl(200, 40%, 77%);
-    --color-tertiary-90: hsl(200, 40%, 88%);
-    --color-tertiary-95: hsl(200, 40%, 94%);
-    --color-tertiary-99: hsl(200, 40%, 98%);
-    --color-neutral-10: hsl(230, 2%, 10%);
-    --color-neutral-20: hsl(230, 2%, 20%);
-    --color-neutral-30: hsl(230, 2%, 30%);
-    --color-neutral-40: hsl(230, 2%, 40%);
-    --color-neutral-50: hsl(230, 2%, 49%);
-    --color-neutral-60: hsl(230, 2%, 59%);
-    --color-neutral-70: hsl(230, 2%, 69%);
-    --color-neutral-80: hsl(230, 2%, 79%);
-    --color-neutral-90: hsl(230, 2%, 89%);
-    --color-neutral-95: hsl(230, 2%, 94%);
-    --color-neutral-99: hsl(230, 2%, 98%);
-    --color-neutral-variant-10: hsl(210, 2%, 10%);
-    --color-neutral-variant-20: hsl(210, 2%, 20%);
-    --color-neutral-variant-30: hsl(210, 2%, 30%);
-    --color-neutral-variant-40: hsl(210, 2%, 40%);
-    --color-neutral-variant-50: hsl(210, 2%, 49%);
-    --color-neutral-variant-60: hsl(210, 2%, 59%);
-    --color-neutral-variant-70: hsl(210, 2%, 69%);
-    --color-neutral-variant-80: hsl(210, 2%, 79%);
-    --color-neutral-variant-90: hsl(210, 2%, 89%);
-    --color-neutral-variant-95: hsl(210, 2%, 94%);
-    --color-neutral-variant-99: hsl(210, 2%, 98%);
-    --color-error-10: hsl(0, 40%, 10%);
-    --color-error-20: hsl(0, 40%, 20%);
-    --color-error-30: hsl(0, 40%, 30%);
-    --color-error-40: hsl(0, 40%, 40%);
-    --color-error-50: hsl(0, 40%, 49%);
-    --color-error-60: hsl(0, 40%, 59%);
-    --color-error-70: hsl(0, 40%, 69%);
-    --color-error-80: hsl(0, 40%, 79%);
-    --color-error-90: hsl(0, 40%, 89%);
-    --color-error-95: hsl(0, 40%, 94%);
-    --color-error-99: hsl(0, 40%, 98%);
   }
 
   body {
@@ -162,6 +165,11 @@ const GlobalStyle = createGlobalStyle`
     background: var(--color-primary-40);
   }
 
+  header .container, footer .container {
+    box-sizing: border-box;
+    padding: 0 1.5rem;
+  }
+
   header a, footer a {
     color: var(--color-white);
   }
@@ -202,12 +210,7 @@ const GlobalStyle = createGlobalStyle`
     flex-direction: column;
     align-items: center;
     width: 100%;
-    min-height: 100vh;
-    background: var(--color-secondary-90);
-  }
-
-  section:nth-child(2n) {
-    background: var(--color-tertiary-90);
+    background: var(--color-neutral-variant-90);
   }
 
   section .container {
