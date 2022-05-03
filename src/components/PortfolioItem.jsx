d09d9c4f095f7259db9b158eba28969432af7e23
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-05-03 21:03:22 +1000

    Add ImageCarousel and pass props.

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -1,5 +1,6 @@
 import TechnologyIcon from './shared/TechnologyIcon';
 import TagsList from './shared/TagsList';
+import ImageCarousel from './shared/ImageCarousel';
 
 const PortfolioItem = ({ item }) => {
   const renderTechnologies = item.technologies.map((technology) => (
@@ -10,6 +11,7 @@ const PortfolioItem = ({ item }) => {
     <div className="portfolio-item">
       <h3>{item.title}</h3>
       <h4>{item.subtitle}</h4>
+      <ImageCarousel title={item.title} images={item.images} />
       <p>{item.description}</p>
       {renderTechnologies}
       <TagsList tags={item.tags} />
