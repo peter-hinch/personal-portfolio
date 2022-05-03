b181e0f4f57a143df49f76695218dc9b03c5cd0d
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-05-03 23:52:23 +1000

    Add fullname property for each Technology.

--- src/enums.js
+++ src/enums.js
@@ -10,29 +10,83 @@ export const devIconsCdnUrl =
 export const Technologies = {
   AMAZONWEBSERVICES: {
     simplename: 'amazonwebservices',
+    fullname: 'Amazon Web Services',
     iconSuffix: '-original-wordmark'
   },
-  ANDROID: { simplename: 'android', iconSuffix: '-original' },
-  AZURE: { simplename: 'azure', iconSuffix: '-original-wordmark' },
-  BOOTSTRAP: { simplename: 'bootstrap', iconSuffix: '-original' },
-  CSHARP: { simplename: 'csharp', iconSuffix: '-original' },
-  CSS3: { simplename: 'css3', iconSuffix: '-original-wordmark' },
-  DOT_NET: { simplename: 'dot-net', iconSuffix: '-original-wordmark' },
-  FIGMA: { simplename: 'figma', iconSuffix: '-original' },
-  FIREBASE: { simplename: 'firebase', iconSuffix: '-plain' },
-  HTML5: { simplename: 'html5', iconSuffix: '-original-wordmark' },
-  JAVA: { simplename: 'java', iconSuffix: '-original-wordmark' },
-  JAVASCRIPT: { simplename: 'javascript', iconSuffix: '-original' },
+  ANDROID: {
+    simplename: 'android',
+    fullname: 'Android OS',
+    iconSuffix: '-original'
+  },
+  AZURE: {
+    simplename: 'azure',
+    fullname: 'Microsoft Azure',
+    iconSuffix: '-original-wordmark'
+  },
+  BOOTSTRAP: {
+    simplename: 'bootstrap',
+    fullname: 'Bootstrap',
+    iconSuffix: '-original'
+  },
+  CSHARP: { simplename: 'csharp', fullname: 'C#', iconSuffix: '-original' },
+  CSS3: {
+    simplename: 'css3',
+    fullname: 'CSS3',
+    iconSuffix: '-original-wordmark'
+  },
+  DOT_NET: {
+    simplename: 'dot-net',
+    fullname: 'Microsoft .NET',
+    iconSuffix: '-original-wordmark'
+  },
+  FIGMA: { simplename: 'figma', fullname: 'Figma', iconSuffix: '-original' },
+  FIREBASE: {
+    simplename: 'firebase',
+    fullname: 'Firebase',
+    iconSuffix: '-plain'
+  },
+  HTML5: {
+    simplename: 'html5',
+    fullname: 'HTML5',
+    iconSuffix: '-original-wordmark'
+  },
+  JAVA: {
+    simplename: 'java',
+    fullname: 'Java',
+    iconSuffix: '-original-wordmark'
+  },
+  JAVASCRIPT: {
+    simplename: 'javascript',
+    fullname: 'JavaScript',
+    iconSuffix: '-original'
+  },
   MICROSOFTSQLSERVER: {
     simplename: 'microsoftsqlserver',
+    fullname: 'Microsoft SQL Server',
     iconSuffix: '-plain-wordmark'
   },
-  MYSQL: { simplename: 'mysql', iconSuffix: '-original-wordmark' },
-  NODEJS: { simplename: 'nodejs', iconSuffix: '-original-wordmark' },
-  REACT: { simplename: 'react', iconSuffix: '-original' },
-  SASS: { simplename: 'sass', iconSuffix: '-original' },
-  TYPESCRIPT: { simplename: 'typescript', iconSuffix: '-original' },
-  WEBPACK: { simplename: 'webpack', iconSuffix: '-original' }
+  MYSQL: {
+    simplename: 'mysql',
+    fullname: 'MySQL',
+    iconSuffix: '-original-wordmark'
+  },
+  NODEJS: {
+    simplename: 'nodejs',
+    fullname: 'Node.js',
+    iconSuffix: '-original-wordmark'
+  },
+  REACT: { simplename: 'react', fullname: 'React.js', iconSuffix: '-original' },
+  SASS: { simplename: 'sass', fullname: 'Sass', iconSuffix: '-original' },
+  TYPESCRIPT: {
+    simplename: 'typescript',
+    fullname: 'TypeScript',
+    iconSuffix: '-original'
+  },
+  WEBPACK: {
+    simplename: 'webpack',
+    fullname: 'Webpack',
+    iconSuffix: '-original'
+  }
 };
 
 export const Social = {
