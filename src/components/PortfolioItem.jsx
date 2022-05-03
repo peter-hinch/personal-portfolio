97d5605e362889fe008f4b69164c36b60b4b5d31
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-05-03 22:26:15 +1000

    Add rendering of site links.

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -3,18 +3,46 @@ import TagsList from './shared/TagsList';
 import ImageCarousel from './shared/ImageCarousel';
 
 const PortfolioItem = ({ item }) => {
+  const imagePath = './../assets/portfolio/';
+
   const renderTechnologies = item.technologies.map((technology) => (
     <TechnologyIcon technology={technology} />
   ));
+
+  const renderLinks = (
+    <>
+      {item.liveUrl !== undefined && (
+        <a
+          className="portfolio-link"
+          href={item.liveUrl}
+          alt="Visit the live site"
+          title="Visit the live site"
+        >
+          Live Site
+        </a>
+      )}
+      {item.sourceUrl !== undefined && (
+        <a
+          className="portfolio-link"
+          href={item.sourceUrl}
+          alt="View the source code on GitHub"
+          title="View the source code on GitHub"
+        >
+          Source Code
+        </a>
+      )}
+    </>
+  );
 
   return (
     <div className="portfolio-item">
       <h3>{item.title}</h3>
       <h4>{item.subtitle}</h4>
-      <ImageCarousel title={item.title} images={item.images} />
+      <ImageCarousel path={imagePath} title={item.title} images={item.images} />
       <p>{item.description}</p>
       {renderTechnologies}
       <TagsList tags={item.tags} />
+      {renderLinks}
     </div>
   );
 };
