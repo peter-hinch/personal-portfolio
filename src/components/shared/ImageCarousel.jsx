bf7eb1f5d95fbb543270ec72e2857c8a5362f683
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-05-03 23:53:45 +1000

    Begin work on ImageCarousel.

--- src/components/shared/ImageCarousel.jsx
+++ src/components/shared/ImageCarousel.jsx
@@ -1,9 +1,15 @@
-const ImageCarousel = ({ images, title }) => {
-  const renderImages = images.map((imageUrl) => (
-    <img src={imageUrl} alt={title} />
-  ));
+import { useEffect } from 'react';
+import { useImage } from './../../hooks/useImage';
 
-  return <>{renderImages}</>;
+const ImageCarousel = ({ path, title, images }) => {
+  useEffect(() => {}, [images]);
+
+  const renderImages = images.map((image) => {
+    const imageUrl = `${path}${image}`;
+    return <img className="carousel-image" src={imageUrl} alt={title} />;
+  });
+
+  return <div className="carousel">{renderImages}</div>;
 };
 
 export default ImageCarousel;
