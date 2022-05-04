76bc741c80d0fe1db6a090de8d6c0772333c13de
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-05-04 11:23:02 +1000

    Add tags list to Technologies.

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,15 +1,19 @@
 import Container from './layout/Container';
+import TagsList from './shared/TagsList';
 import TechnologyIcons from './shared/TechnologyIcons';
 
-const Technologies = ({ preferredTechnologies }) => (
+const Technologies = ({ preferredTechnologies, otherTechnologies }) => (
   <section id="technologies">
     <Container>
       <h2>Technologies</h2>
-      <h3>Preferred Technologies</h3>
+      <h3>I can work with these</h3>
       <div className="technologies-info">
         <TechnologyIcons technologies={preferredTechnologies.technologies} />
       </div>
-      <h3>Other Experience</h3>
+      <h3>As well as these</h3>
+      <div className="technologies-info">
+        <TagsList tags={otherTechnologies.tags} />
+      </div>
     </Container>
   </section>
 );
