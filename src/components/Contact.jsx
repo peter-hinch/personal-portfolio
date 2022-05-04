f9acae7f87c80f0f5ba040bbd1d19f4d5fa64e50
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-05-05 01:47:25 +1000

    Add ContactForm.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,5 @@
 import Container from './layout/Container';
+import ContactForm from './layout/ContactForm';
 import ParagraphArray from './shared/ParagraphArray';
 
 const Contact = ({ contact }) => {
@@ -7,7 +8,9 @@ const Contact = ({ contact }) => {
       <Container>
         <h2>Contact</h2>
         <ParagraphArray textArray={contact} />
-        <div className="breakout-info"></div>
+        <div className="breakout-info">
+          <ContactForm />
+        </div>
       </Container>
     </section>
   );
