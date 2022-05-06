a0e0969b483b2d423636c21bbb2eca95b4bc5ef1
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-05-06 13:16:44 +1000

    Add classNames for styling.

--- src/components/layout/ContactForm.jsx
+++ src/components/layout/ContactForm.jsx
@@ -2,18 +2,22 @@ const getformToken = process.env.REACT_APP_GETFORM_TOKEN;
 
 const ContactForm = () => {
   return (
-    <form action={`https://getform.io/f/${getformToken}`} method="POST">
+    <form
+      className="contact-form"
+      action={`https://getform.io/f/${getformToken}`}
+      method="POST"
+    >
       <div className="form-fieldset">
         <label htmlFor="contact-name">Name</label>
-        <input id="contact-name" type="text" name="name" />
+        <input id="contact-name" type="text" name="name" required />
       </div>
       <div className="form-fieldset">
         <label htmlFor="contact-email">Email</label>
-        <input id="contact-email" type="text" name="email" />
+        <input id="contact-email" type="email" name="email" required />
       </div>
       <div className="form-fieldset">
         <label htmlFor="contact-message">Message</label>
-        <textarea id="contact-message" name="message" />
+        <textarea id="contact-message" name="message" required />
       </div>
       <button type="submit">Submit</button>
     </form>
