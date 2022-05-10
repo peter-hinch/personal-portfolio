1945a2e90b84f88c9842d8e1204dad2d6c4e2896
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-05-10 15:20:59 +1000

    Add PUBLIC_URL pefix to image path.

--- src/components/shared/Image.jsx
+++ src/components/shared/Image.jsx
@@ -1,5 +1,9 @@
 const Image = ({ path, alt }) => (
-  <img className="carousel-image" src={`images/${path}`} alt={alt} />
+  <img
+    className="carousel-image"
+    src={`${process.env.PUBLIC_URL}/images/${path}`}
+    alt={alt}
+  />
 );
 
 export default Image;
