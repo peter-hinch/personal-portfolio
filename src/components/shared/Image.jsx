97bb23e4200fa401dc7448a26dd64d061eeb5c7a
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2023-01-16 18:05:55 +1100

    Add image container.

--- src/components/shared/Image.jsx
+++ src/components/shared/Image.jsx
@@ -1,9 +1,20 @@
+import styled from 'styled-components';
+
+const StyledImgContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  min-width: 100%;
+`;
+
 const Image = ({ path, alt }) => (
-  <img
-    className="carousel-image"
-    src={`${process.env.PUBLIC_URL}/images/${path}`}
-    alt={alt}
-  />
+  <StyledImgContainer>
+    <img
+      className="carousel-image"
+      src={`${process.env.PUBLIC_URL}/images/${path}`}
+      alt={alt}
+    />
+  </StyledImgContainer>
 );
 
 export default Image;
