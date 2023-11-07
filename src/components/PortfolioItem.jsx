5afe517f8c2f692b76ff7b51fd5b2e9b6584854b
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2023-11-07 16:16:38 +1100

    Add NextLink component to portfolio items.

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -1,42 +1,44 @@
 import TechnologyIcons from './shared/TechnologyIcons';
 import TagsList from './shared/TagsList';
 import ImageCarousel from './shared/ImageCarousel';
+import NextLink from './shared/NextLink';
 
 const PortfolioItem = ({ item }) => {
   const renderLinks = (
-    <div className="cta-links">
-      <div className="cta-links--group">
-        {item.liveUrl !== undefined && (
-          <a
-            className="cta-link"
-            href={item.liveUrl}
-            alt="Visit the live site"
-            title="Visit the live site"
-          >
-            Visit live site
-          </a>
-        )}
-        {item.sourceUrl !== undefined && (
-          <a
-            className="cta-link"
-            href={item.sourceUrl}
-            alt="View the source code on GitHub"
-            title="View the source code on GitHub"
-          >
-            View source code
-          </a>
-        )}
-      </div>
-    </div>
+    <>
+      {item.liveUrl !== undefined && (
+        <a
+          className="cta-link"
+          href={item.liveUrl}
+          alt="Visit the live site"
+          title="Visit the live site"
+        >
+          Visit live site
+        </a>
+      )}
+      {item.sourceUrl !== undefined && (
+        <a
+          className="cta-link"
+          href={item.sourceUrl}
+          alt="View the source code on GitHub"
+          title="View the source code on GitHub"
+        >
+          View source code
+        </a>
+      )}
+    </>
   );
 
   return (
-    <div className="portfolio-item">
+    <div id={item.id ?? item.id} className="portfolio-item">
       <h3>{item.title}</h3>
       <h4>{item.subtitle}</h4>
       <ImageCarousel title={item.title} images={item.images} />
       <p>{item.description}</p>
-      {renderLinks}
+      <div className="cta-links">
+        <div className="cta-links--group">{renderLinks}</div>
+        <NextLink portfolioItem={item} />
+      </div>
       <div className="breakout-info">
         <TechnologyIcons technologies={item.technologies} />
         <TagsList tags={item.tags} />
