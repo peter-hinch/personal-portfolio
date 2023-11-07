17b72e4b57a6b753802ba600a42f4f6782f014f6
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2023-11-07 16:18:07 +1100

    Add portfolio item capability to NextLink.

--- src/components/shared/NextLink.jsx
+++ src/components/shared/NextLink.jsx
@@ -1,44 +1,57 @@
-import { anchors } from '../../portfolioData';
+import { anchors, portfolioItems } from '../../portfolioData';
 
 import styled from 'styled-components';
 
 import imgCaret from './../../assets/images/caret.svg';
 
-// TODO: Find next anchor in sequence.
-const getNextAnchor = (anchor) => {
+const getSortedAnchorsArray = () => {
   // Convert anchors object into an array
   const anchorsAsArray = [];
   for (var key in anchors) {
     anchorsAsArray.push(anchors[key]);
   }
   // Sort by sequence
-  const sortedAnchorsArray = anchorsAsArray.sort(
-    (a, b) => a.sequence > b.sequence
-  );
-  // Find index of the current anchor
-  const anchorIndex = sortedAnchorsArray.findIndex(
-    (item) => item.sequence === anchor.sequence
-  );
-  // Return next anchor, or a link to the first anchor
-  // if the end has been reached
-  if (anchorIndex < sortedAnchorsArray.length - 1) {
-    return sortedAnchorsArray[anchorIndex + 1];
-  } else {
-    return sortedAnchorsArray[0];
+  return anchorsAsArray.sort((a, b) => a.sequence > b.sequence);
+};
+
+const getNextItemIndex = (referenceItem, array) => {
+  // Find the index of the current item
+  const itemIndex = array.findIndex((item) => item.id === referenceItem.id);
+  // Return the next item, or first item if the end has been reached
+  return itemIndex < array.length - 1 ? itemIndex + 1 : 0;
+};
+
+const getNextLinkItem = (anchor, portfolioItem) => {
+  if (anchor !== null) {
+    const anchorsArray = getSortedAnchorsArray(anchor);
+    const nexItemIndex = getNextItemIndex(anchor, anchorsArray);
+    return anchorsArray[nexItemIndex];
+  } else if (portfolioItem !== null) {
+    const nexItemIndex = getNextItemIndex(portfolioItem, portfolioItems);
+    return portfolioItems[nexItemIndex];
   }
 };
 
-const NextLink = ({ anchor }) => {
-  const nextAnchor = getNextAnchor(anchor);
+const determineLastItem = (anchor, portfolioItem) => {
+  if (anchor !== null) {
+    const anchorsArray = getSortedAnchorsArray(anchor);
+    return getNextItemIndex(anchor, anchorsArray) === 0 ? true : false;
+  } else if (portfolioItem !== null) {
+    return getNextItemIndex(portfolioItem, portfolioItems) === 0 ? true : false;
+  }
+};
+
+const NextLink = ({ anchor = null, portfolioItem = null }) => {
+  const nextLinkItem = getNextLinkItem(anchor, portfolioItem);
+  const isLastastItem = determineLastItem(anchor, portfolioItem);
+
   return (
     <StyledNextLink>
       <a
-        className={`cta-link cta-link--${
-          nextAnchor.sequence !== 0 ? 'next' : 'home'
-        }`}
-        href={`#${nextAnchor.id}`}
+        className={`cta-link cta-link--${!isLastastItem ? 'next' : 'home'}`}
+        href={`#${nextLinkItem.id}`}
       >
-        {nextAnchor.title}
+        {`${!isLastastItem ? 'Next' : 'Back'}: ${nextLinkItem.title}`}
       </a>
     </StyledNextLink>
   );
