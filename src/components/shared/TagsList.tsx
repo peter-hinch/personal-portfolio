5523af3627dcbb25b43a60e6f687b03cb8490d3d
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2024-11-06 08:25:47 +1100

    Refsctor for simplicity.

--- src/components/shared/TagsList.tsx
+++ src/components/shared/TagsList.tsx
@@ -1,13 +1,15 @@
 import styled from 'styled-components';
 
 const TagsList: React.FC<{ tags: string[] }> = ({ tags }) => {
-  const renderTags = tags.map((tag) => (
-    <li key={`tags-${tag}`} className="tag">
-      {tag}
-    </li>
-  ));
-
-  return <StyledTagsList className="tags-list">{renderTags}</StyledTagsList>;
+  return (
+    <StyledTagsList className="tags-list">
+      {tags.map((tag) => (
+        <li key={`tags-${tag}`} className="tag">
+          {tag}
+        </li>
+      ))}
+    </StyledTagsList>
+  );
 };
 
 const StyledTagsList = styled.ul`
@@ -23,11 +25,11 @@ const StyledTagsList = styled.ul`
 
     li.tag {
       flex-shrink: 0;
-      padding: 0.25rem 0.75rem 0;
+      padding: 0.125rem 0.75rem;
       height: 1.25rem;
       color: var(--theme-on-secondary);
       background: var(--theme-secondary);
-      border-radius: 0.75rem;
+      border-radius: 0.125rem;
 
       &:last-child {
         margin-right: 0;
