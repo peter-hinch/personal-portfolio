a99ee84280b3dffad3e801024c79fff2801f08e5
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2024-11-06 08:24:47 +1100

    Move some styling from GlobalStyle.

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -1,3 +1,5 @@
+import styled from 'styled-components';
+
 import Container from './layout/Container';
 import ParagraphArray from './shared/ParagraphArray';
 import TagsList from './shared/TagsList';
@@ -9,26 +11,34 @@ const Technologies: React.FC<{
   preferredTechnologies: PortfolioData.PreferredTechnologies;
   otherTechnologies: PortfolioData.OtherTechnologies;
 }> = ({ anchor, preferredTechnologies, otherTechnologies }) => (
-  <Container id="technologies">
-    <h2>Technologies</h2>
-    <h3>{preferredTechnologies.heading}</h3>
-    <ParagraphArray textArray={preferredTechnologies.description} />
-    <div className="breakout-info">
-      <TechnologyIcons
-        technologies={preferredTechnologies.technologies}
-        size={5}
-      />
-    </div>
-    <h3>{otherTechnologies.heading}</h3>
-    <ParagraphArray textArray={otherTechnologies.description} />
-    <div className="breakout-info">
-      <TagsList tags={otherTechnologies.tags} />
-    </div>
-    <div className="cta-links">
-      <div className="cta-links--group"></div>
-      <NextLink anchor={anchor} />
-    </div>
-  </Container>
+  <StyledTechnologies>
+    <Container id="technologies">
+      <h2>Technologies</h2>
+      <h3>{preferredTechnologies.heading}</h3>
+      <ParagraphArray textArray={preferredTechnologies.description} />
+      <div className="breakout-info">
+        <TechnologyIcons
+          technologies={preferredTechnologies.technologies}
+          size={5}
+        />
+      </div>
+      <h3>{otherTechnologies.heading}</h3>
+      <ParagraphArray textArray={otherTechnologies.description} />
+      <div className="breakout-info">
+        <TagsList tags={otherTechnologies.tags} />
+      </div>
+      <div className="cta-links">
+        <div className="cta-links--group"></div>
+        <NextLink anchor={anchor} />
+      </div>
+    </Container>
+  </StyledTechnologies>
 );
+
+const StyledTechnologies = styled.div`
+  #technologies .breakout-info .tags-list {
+    margin-top: 0;
+  }
+`;
 
 export default Technologies;
