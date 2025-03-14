25bd26b16576176f2a851939a1ea55931333c34e
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2025-03-14 23:28:15 +1100

    Simplify links render.

--- src/components/shared/SocialLinks.tsx
+++ src/components/shared/SocialLinks.tsx
@@ -1,22 +1,35 @@
+import styled from 'styled-components';
+
 import { devIconsCdnUrl } from '../../enums';
 
 const SocialLinks: React.FC<{
   myName: string;
   socialLinks: PortfolioData.SocialLink[];
 }> = ({ myName, socialLinks }) => {
-  const renderSocialLinks = socialLinks.map((link) => (
-    <li key={`social-link-${link.platform.simplename}`}>
-      <a href={link.url}>
-        <img
-          className="social-link"
-          src={`${devIconsCdnUrl}${link.platform.simplename}/${link.platform.simplename}${link.platform.iconSuffix}.svg`}
-          alt={`${myName} on ${link.platform.fullname}`}
-        />
-      </a>
-    </li>
-  ));
-
-  return <ul>{renderSocialLinks}</ul>;
+  return (
+    <StyledSocialLinks className="social-links">
+      {socialLinks.map((link) => (
+        <li key={`social-link-${link.platform.simplename}`}>
+          <a href={link.url}>
+            <img
+              className="social-links--link"
+              src={`${devIconsCdnUrl}${link.platform.simplename}/${link.platform.simplename}${link.platform.iconSuffix}.svg`}
+              alt={`${myName} on ${link.platform.fullname}`}
+            />
+          </a>
+        </li>
+      ))}
+    </StyledSocialLinks>
+  );
 };
 
 export default SocialLinks;
+
+const StyledSocialLinks = styled.ul`
+  a:has(img.social-links--link) {
+    display: flex;
+    img {
+      height: 1.5rem;
+    }
+  }
+`;
