fff3225d8b3fb631751f81cf504c94395ffa99e5
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2025-03-14 23:45:11 +1100

    Allow passing of "highlight" tags to be shown first.

--- src/components/shared/TagsList.tsx
+++ src/components/shared/TagsList.tsx
@@ -1,13 +1,39 @@
+import { useState } from 'react';
+
 import styled from 'styled-components';
 
-const TagsList: React.FC<{ tags: string[] }> = ({ tags }) => {
+const TagsList: React.FC<{
+  tags: string[];
+  qty?: number;
+  highlight?: string[];
+}> = ({ tags, qty = 5, highlight = [] }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const orderedTags = [
+    ...highlight,
+    ...tags.filter((t) => !highlight.includes(t))
+  ];
+  const remainingTags = orderedTags.slice(qty, tags.length - 1);
+
+  const renderTag = (tag: string) => (
+    <li key={`tag-${tag}`} className="tag">
+      <div>{tag}</div>
+    </li>
+  );
+
   return (
     <StyledTagsList className="tags-list">
-      {tags.map((tag) => (
-        <li key={`tags-${tag}`} className="tag">
-          {tag}
-        </li>
-      ))}
+      {orderedTags.map((tag, index) => index < qty && renderTag(tag))}
+      {remainingTags.length > 0 && (
+        <>
+          {isExpanded && remainingTags.map((tag) => renderTag(tag))}
+          <li key={`tags-expand`} className="tag">
+            <button onClick={() => setIsExpanded(!isExpanded)}>
+              {!isExpanded ? `+${remainingTags.length} more` : 'show less'}
+            </button>
+          </li>
+        </>
+      )}
     </StyledTagsList>
   );
 };
@@ -25,14 +51,21 @@ const StyledTagsList = styled.ul`
 
     li.tag {
       flex-shrink: 0;
-      padding: 0.125rem 0.75rem;
-      height: 1.25rem;
       color: var(--theme-on-secondary);
       background: var(--theme-secondary);
-      border-radius: 0.125rem;
+      border-radius: 0.5rem;
+      user-select: none;
 
-      &:last-child {
-        margin-right: 0;
+      div,
+      button {
+        padding: 0.2rem 0.75rem 0.25rem;
+      }
+
+      button {
+        color: inherit;
+        border: none;
+        background: transparent;
+        cursor: pointer;
       }
     }
   }
